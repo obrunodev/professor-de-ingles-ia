58a5709791e38841c30a53bb7a7da93b2b2ffd3b
Bruno R. Pianca <brunorpdev@gmail.com>
2024-11-21 09:32:38 -0300

    fix: localhost host

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['fhopzn.hospedagemelastica.com.br']
+ALLOWED_HOSTS = ['fhopzn.hospedagemelastica.com.br', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['https://fhopzn.hospedagemelastica.com.br']
 
 INSTALLED_APPS = [
