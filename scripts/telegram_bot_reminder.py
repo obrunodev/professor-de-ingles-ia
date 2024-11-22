035cd9f3253328efb1ca3e48ec712bca265a2310
Bruno R. Pianca <brunorpdev@gmail.com>
2024-11-22 01:54:48 -0300

    feat: Gerando dicas com o GPT para enviar via Cron

--- scripts/telegram_bot_reminder.py
+++ scripts/telegram_bot_reminder.py
@@ -18,10 +18,25 @@ client = OpenAI(api_key=OPENAI_TOKEN)
 
 
 def main():
+    response = client.chat.completions.create(
+        model='gpt-4o-mini',
+        messages=[
+            {
+                'role': 'system',
+                'content': 'Você é um tutor de inglês amigável e didático, especializado em ajudar alunos de todos os níveis a aprenderem inglês de forma prática e eficiente. Forneça dicas úteis, ensine novas palavras ou frases, explique regras gramaticais, ou sugira exercícios criativos. Sempre adapte suas respostas ao nível de inglês do aluno, usando exemplos claros e mantendo o aprendizado envolvente. Certifique-se de encorajar e motivar o aluno em cada interação.',
+            },
+            {
+                'role': 'user',
+                'content': 'Preciso de uma dica prática para aprender inglês ou de algo novo para aprender hoje. Pode ser palavras, frases, uma explicação gramatical ou um exercício simples para praticar.',
+            },
+        ],
+    )
+    response_text = response.choices[0].message.content
+
     students = Student.objects.filter(telegram_chat_id__isnull=False)
     for student in students:
         chat_id = student.telegram_chat_id
-        bot.send_message(chat_id=chat_id, text="Uma mensagem de teste")
+        bot.send_message(chat_id=chat_id, text=response_text)
 
 
 if __name__ == '__main__':
